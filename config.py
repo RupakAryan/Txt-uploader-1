e6163a98473c71b3b34e1b4803e0550b11d61ae7
RupakAryan <157547044+RupakAryan@users.noreply.github.com>
2025-12-26 05:39:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 import os
 
-API_ID = API_ID =  
+API_ID = API_ID =  29978901
 
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "500fc876c5356cf04ed3698912dc2edf")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8464714544:AAF7cN9yIE_JoLap0HsjXlEhuGCKspr-jtw")
 
-PASS_DB = int(os.environ.get("PASS_DB", "721"))
+PASS_DB = int(os.environ.get("PASS_DB", "mongodb+srv://rupakaryanaryan:rupakaryanaryan@cluster0.05xkjx2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"))
 
 OWNER = int(os.environ.get("OWNER", ))
 
@@ -18,7 +18,7 @@ LOG = ,
 
 try:
     ADMINS=[]
-    for x in (os.environ.get("ADMINS", "502980590").split()):
+    for x in (os.environ.get("ADMINS", "5776977809").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
